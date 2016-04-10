7c28e271c080a4e18268ceec9bfbe277f77ba6b1
Minku Lee <premist@me.com>
2016-04-10 17:24:46 +0900

    Follow Polymer tutorial (#1)
    * Add basic awesome-meme element files
    
    * Add basic markup alongside with its CSS styling
    
    * Add AwesomeMeme Polymer definition using ES2015
    
    * Convert my-greeting element to use ES2015
    
    * Convert <my-list> to use ES2015
    
    * Use created <awesome-meme> element on the app.
    
    Tutorial finished!
    
    * Use ES2015 arrow functions on routing.html
    
    * Use ES2015 let and template strings on app.js

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -13,8 +13,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  
+  let app = document.querySelector('#app');
+
   // Sets app default base URL
   app.baseUrl = '/';
   if (window.location.port === '') {  // if production
@@ -35,7 +35,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   app.addEventListener('dom-change', function() {
     console.log('Our app is ready to rock!');
   });
-  
+
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
@@ -46,27 +46,27 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   // The appName is moved to top and shrunk on condensing. The bottom sub title
   // is shrunk to nothing on condensing.
   window.addEventListener('paper-header-transform', function(e) {
-    var appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
-    var middleContainer = Polymer.dom(document).querySelector('#mainToolbar .middle-container');
-    var bottomContainer = Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
-    var detail = e.detail;
-    var heightDiff = detail.height - detail.condensedHeight;
-    var yRatio = Math.min(1, detail.y / heightDiff);
+    let appName = Polymer.dom(document).querySelector('#mainToolbar .app-name');
+    let middleContainer = Polymer.dom(document).querySelector('#mainToolbar .middle-container');
+    let bottomContainer = Polymer.dom(document).querySelector('#mainToolbar .bottom-container');
+    let detail = e.detail;
+    let heightDiff = detail.height - detail.condensedHeight;
+    let yRatio = Math.min(1, detail.y / heightDiff);
     // appName max size when condensed. The smaller the number the smaller the condensed size.
-    var maxMiddleScale = 0.50;
+    let maxMiddleScale = 0.50;
     var auxHeight = heightDiff - detail.y;
     var auxScale = heightDiff / (1 - maxMiddleScale);
-    var scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
-    var scaleBottom = 1 - yRatio;
+    let scaleMiddle = Math.max(maxMiddleScale, auxHeight / auxScale + maxMiddleScale);
+    let scaleBottom = 1 - yRatio;
 
     // Move/translate middleContainer
-    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
+    Polymer.Base.transform(`translate3ed(0,${yRatio * 100},0)`, middleContainer);
 
     // Scale bottomContainer and bottom sub title to nothing and back
-    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
+    Polymer.Base.transform(`scale(${scaleBottom}) translateZ(0)`, bottomContainer);
 
     // Scale middleContainer appName
-    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
+    Polymer.Base.transform(`scale(${scaleMiddle}) translateZ(0)`, appName);
   });
 
   // Scroll page to top and expand header
